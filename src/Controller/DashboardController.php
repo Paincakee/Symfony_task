d6720bc7a691d3d1be04eda4cbb59242fa53a227
Richard Vullings <rvullings8@gmail.com>
2024-03-13 16:59:01 +0100

    Layout done

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,7 +12,8 @@ class DashboardController extends AbstractController
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'title' => 'Dashboard',
+            'icon' => 'speedometer2'
         ]);
     }
 }
