21d3a801fdb7a9d96aa75e3545c5b9ace69b29ba
Richard Vullings <rvullings8@gmail.com>
2024-03-14 17:26:55 +0100

    Form almost done (task)

--- src/Controller/System/RegistrationController.php
+++ src/Controller/System/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\System;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,7 +18,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Controller/taskManagement/TaskController.php
+++ src/Controller/taskManagement/TaskController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\taskManagement;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,11 +22,27 @@ class TaskController extends AbstractController
     }
 
     #[Route('/task_create', name: 'app_task_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('task/index.html.twig', [
+        // Get the current user
+        $user = $this->getUser();
+
+        $task = new Task();
+
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('task/create.html.twig', [
             'title' => 'Create task',
-            'icon' => 'plus-circle-dotted'
+            'icon' => 'plus-circle-dotted',
+            'taskForm' => $form->createView(),
         ]);
     }
 }
