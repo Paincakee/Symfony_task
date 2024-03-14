dcd3b84e83cf47f0b8e383507158f364c18167a3
Richard Vullings <rvullings8@gmail.com>
2024-03-14 15:39:43 +0100

    Dropdown sidebar || Admin crud optimisation

--- src/Controller/taskManagement/TaskController.php
+++ src/Controller/taskManagement/TaskController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
+    #[Route('/tasks', name: 'app_task_view')]
     public function index(): Response
     {
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'title' => 'Tasks',
+            'icon' => 'columns-gap'
+        ]);
+    }
+
+    #[Route('/task_create', name: 'app_task_create')]
+    public function create(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'title' => 'Create task',
+            'icon' => 'plus-circle-dotted'
         ]);
     }
 }
