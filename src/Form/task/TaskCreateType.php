6b0f78531ec3b2d462fb1fb259a254d5d938f9f9
Richard Vullings <rvullings8@gmail.com>
2024-03-21 16:26:47 +0100

    Project detail styling || Task Update form || New task service function

---
+++ src/Form/task/TaskCreateType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form\task;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskCreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email',
+
+            ]);
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            // Check if the form data is null or if it doesn't have a user property
+            if (null === $data || !property_exists($data, 'user')) {
+                // If so, remove the user field from the form
+                $form->remove('user');
+            }
+        });
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
